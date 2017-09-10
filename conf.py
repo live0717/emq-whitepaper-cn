ed6fe729b19a7d5dd16a9ac9a3072ac9cd5bc547
Feng Lee <feng@emqtt.io>
2017-09-11 06:44:54 +0800

    Mv whitepaper.rst to index.rst

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'whitepaper'
+master_doc = 'index'
 
 # General information about the project.
 project = u'EMQ 消息服务器产品白皮书'
